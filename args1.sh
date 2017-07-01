04f620ab0aa03706213c1a851cccbc9106979941
root <root@ip-172-31-22-107.us-east-2.compute.internal>
2017-07-01 10:46:37 -0400

    revisit

--- args1.sh
+++ args1.sh
@@ -1,9 +1,9 @@
-git add $1
-git commit -m $2
 
-if [ -f $1 ]
+if [ ! -f $1 ]
 then
-   git push 
-else
    echo Error: File not found
+else
+   git add $1
+   git commit -m $2
+   git push mygithub
 fi 
